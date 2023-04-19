36147a21c92c1cbc84b3a6a9a06d3e32f100d3b6
Blaz1x <tguyguy22@gmail.com>
2023-04-19 03:36:35 +0300

    release v2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from tradingview_ta import TA_Handler, Interval, Exchange
+from tradingview_ta import TA_Handler
+from datetime import datetime
 import requests
 import json
 import time
@@ -22,20 +23,28 @@ def main():
 
             r = requests.get('https://www.binance.com/fapi/v1/ticker/price?symbol=ETHUSDT')
             text = json.loads(r.text)
+            
             SYMBOL_NAME = text["symbol"]
             SYMBOL_PRICE = text["price"]
+            SYMBOL_TIME_TIMESTAMP = int(text["time"])
+            SYMBOL_TIME = datetime.fromtimestamp(SYMBOL_TIME_TIMESTAMP / 1000).strftime("%H:%M:%S")
             SYMBOL_TOTAL = SYMBOL_NAME+ ': ' + SYMBOL_PRICE + ' $'
-            print(SYMBOL_TOTAL)
+            
+            print(f"{SYMBOL_TOTAL} AT {SYMBOL_TIME}")
             print('RECOMMENDATION:', RECOMMENDATION)
             print('-'*60)
+            
             last_price.append(SYMBOL_PRICE)
             MAX_PRICE = float(max(last_price))
             MIN_PRICE = float(min(last_price))
-            print(f'{SYMBOL_NAME} MAX PRICE:', MAX_PRICE)
-            print(f'{SYMBOL_NAME} MIN PRICE:', MIN_PRICE)
+            
+            print(f'{SYMBOL_NAME} MAX PRICE:{MAX_PRICE}')
+            print(f'{SYMBOL_NAME} MIN PRICE:{MIN_PRICE}')
+            
             PERECENT_DIF = abs(MAX_PRICE - MIN_PRICE) / ((MAX_PRICE + MIN_PRICE) / 2) * 100
             PERECENT_DIF_ROUNDED = round(PERECENT_DIF, 2)
             print(f"{SYMBOL_NAME} PRICE DIFFERENCE BETWEEN {MAX_PRICE} & {MIN_PRICE} is - {PERECENT_DIF_ROUNDED}%")
+            
             print('-'*60)
             time.sleep(60)
         except Exception as ex:
