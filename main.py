a63031f22dce767a035b4b21a41097bc98221236
Blaz1x <tguyguy22@gmail.com>
2023-04-19 03:46:14 +0300

    release v2.0.2

--- main.py
+++ main.py
@@ -32,20 +32,20 @@ def main():
             
             print(f"{SYMBOL_TOTAL} AT {SYMBOL_TIME}")
             print('RECOMMENDATION:', RECOMMENDATION)
-            print('-'*60)
+            print('-'*63)
             
             last_price.append(SYMBOL_PRICE)
             MAX_PRICE = float(max(last_price))
             MIN_PRICE = float(min(last_price))
             
-            print(f'{SYMBOL_NAME} MAX PRICE:{MAX_PRICE}$')
-            print(f'{SYMBOL_NAME} MIN PRICE:{MIN_PRICE}$')
+            print(f'{SYMBOL_NAME} MAX PRICE: {MAX_PRICE}$')
+            print(f'{SYMBOL_NAME} MIN PRICE: {MIN_PRICE}$')
             
             PERECENT_DIF = abs(MAX_PRICE - MIN_PRICE) / ((MAX_PRICE + MIN_PRICE) / 2) * 100
             PERECENT_DIF_ROUNDED = round(PERECENT_DIF, 2)
-            print(f"{SYMBOL_NAME} PRICE DIFFERENCE BETWEEN {MAX_PRICE} & {MIN_PRICE} is - {PERECENT_DIF_ROUNDED}%")
+            print(f"{SYMBOL_NAME} PRICE DIFFERENCE BETWEEN {MAX_PRICE}$ & {MIN_PRICE}$ IS - {PERECENT_DIF_ROUNDED}%")
             
-            print('-'*60)
+            print('-'*63)
             time.sleep(60)
         except Exception as ex:
             print(ex)
